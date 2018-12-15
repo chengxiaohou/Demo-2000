37f017aaa230ed15d19ff7f0e25c2f9b06b0c1df
橙晓侯 <1019890763@qq.com>
2018-12-15 10:12:10 +0800

    Initial Commit

---
+++ Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Demo
+//
+//  Created by 橙晓侯 on 2018/12/15.
+//  Copyright © 2018 橙晓侯. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Demo/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  Demo
+//
+//  Created by 橙晓侯 on 2018/12/15.
+//  Copyright © 2018 橙晓侯. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
